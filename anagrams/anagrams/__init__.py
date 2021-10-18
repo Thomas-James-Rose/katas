14961079cfb870536e16259c55eee351d41042f5
Tom Rose <9136302+Thomas-James-Rose@users.noreply.github.com>
2021-10-18 11:21:02 +0100

    Only search words of the same length for anagrams

--- anagrams/anagrams/__init__.py
+++ anagrams/anagrams/__init__.py
@@ -1,13 +1,26 @@
 from . import words
+from functools import reduce
+
+def sort_word_by_length (acc, word):
+  word_length = len(word)
+  if(word_length in acc):
+    acc[word_length].append(word)
+  else:
+    acc[word_length] = [word]
+
+  return acc
+  
 
 words_file = open("./words.txt", "r")
 word_list =  words_file.read().splitlines()
+word_list_by_word_length = reduce(sort_word_by_length, word_list, {})
 
 def get_anagrams(word):
   if word not in word_list:
     raise RuntimeError('Word not found in dictionary')
+
   anagram_filter = words.get_anagram_filter(word)
-  anagrams = list(filter(anagram_filter, word_list))
+  anagrams = list(filter(anagram_filter, word_list_by_word_length[len(word)]))
   return anagrams
 
 def get_all_anagrams():
